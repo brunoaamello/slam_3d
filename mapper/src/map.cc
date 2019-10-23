1cf61b8217cc5a4f90ac31625de78579b1b9e2b9
GuttinRibeiro <augustoribeiro97@gmail.com>
2019-10-23 11:20:05 -0300

    Now mapper can publish a transform

--- mapper/src/map.cc
+++ mapper/src/map.cc
@@ -1,5 +1,6 @@
 // Ros libraries
 #include "ros/ros.h"
+#include "tf/transform_broadcaster.h"
 
 //Own codes
 #include "scanner/lidar_data.h"
@@ -18,11 +19,19 @@ int main (int argc, char **argv) {
     ros::Subscriber sub_lidar = n.subscribe("/lidar_data", 1000000, &Mapper::lidarCallback, &map);
     ros::Subscriber sub_servo = n.subscribe("/servo_control", 1000, &Mapper::servoCallback, &map);
     ros::Publisher mapper_pub = n.advertise<pcl::PointCloud<pcl::PointXYZ> >("cloud_in", 10);
+    
+    // Para publicar a transformação de referencial:
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+    transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
+    tf::Quaternion q;
+    q.setRPY(0, 0, 0);
+    transform.setRotation(q);
+
     ros::Rate loop_rate(0.5);
 
-    //rosrun tf static_transform_publisher 0 0 0 0 0 1 transform_reference  my_frame 100
-
     while(ros::ok()) {
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "transform_reference", "my_frame")); 
         std::cout << "Numero de pacotes lidar: " << map.getLidarCount() << "\n";
         pcl::PointCloud<pcl::PointXYZ>::Ptr msg = map.getPointCloudMessage(&msgIsEmpty);
         if(!msgIsEmpty) {
