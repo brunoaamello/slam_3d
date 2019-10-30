4652a8938eb2e772eb6002995674c388d3888c31
GuttinRibeiro <augustoribeiro97@gmail.com>
2019-10-30 12:28:32 -0300

    Adding robot controller node

---
+++ robot_ctr/src/main.cc
@@ -0,0 +1,23 @@
+// Ros libraries
+#include "ros/ros.h"
+
+//Own codes
+#include "robot_input/velocity_message.h"
+#include "classes/controller.hh"
+
+// External libraries
+#include <iostream>
+
+int main(int argc, char **argv) {
+        
+    ros::init(argc, argv, "robot_ctr");
+    ros::NodeHandle n;
+    std::cout << "Robot controller running!\n";
+    Controller ctr;
+
+    ros::Subscriber sub = n.subscribe("robot_velocity", 1000, &Controller::robotVelocityCallback, &ctr);
+
+    ros::spin();
+
+    return 0;
+}
